9487d37ab7c50daf6f1ec9c9f5f28749726a1aa9
Marta Kozak <ma.maaravi@gmail.com>
2019-09-15 21:53:30 +0300

    mastered 1 lab via reduce()

--- 1_loops.py
+++ 1_loops.py
@@ -1,3 +1,4 @@
+#sum=sin(x)+sin(x^2)+xin(x^3)+...+sin(x^n)
 from math import sin
 sum=0
 
@@ -8,3 +9,7 @@ for i in range(1,n+1):
     sum+=sin(x**i)
 
 print("Sum = ", sum)
+
+from functools import reduce
+
+print(reduce(lambda a, x: a+x, [sin(x**i) for i in range(1, n+1)]))
